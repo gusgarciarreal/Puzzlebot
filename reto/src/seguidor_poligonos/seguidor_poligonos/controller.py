d28fce5bb99714ba64122335b90f8ef5d4697105
JossueE <kadavetar10@icloud.com>
2025-04-23 19:52:22 -0600

    Odometry with State Machine

--- reto/src/seguidor_poligonos/seguidor_poligonos/controller.py
+++ reto/src/seguidor_poligonos/seguidor_poligonos/controller.py
@@ -42,97 +42,66 @@ class Controller(Node):
         self.current_y = 0.0     # Current y-coordinate position of the robot.
         self.current_yaw = 0.0   # Current orientation (yaw angle in radians).
 
+        # Initialize the range of error and the begin of state machine
+        self.tolerance = 0.05  # Umbral de tolerancia en radianes
+        self.state = "IDLE"  # It could be "IDLE", "ROTATING", "MOVING"
+        self.current_target = None
+
+        #Queue to manage data from ExtendedPose
+        self.pose_queue = []  
+
     def odom_callback(self, msg):
-        self.current_x  = msg.pose.pose.position.x
-        self.current_y = msg.pose.pose.position.y
 
         q = msg.pose.pose.orientation
         quaternion = [q.w, q.x, q.y, q.z]
         euler = transforms3d.euler.quat2euler(quaternion)
 
         self.current_yaw = euler[2]
+        self.current_x  = msg.pose.pose.position.x
+        self.current_y = msg.pose.pose.position.y
 
         self.get_logger().info(f'Received odometry: X={self.current_x:.2f}, Y={self.current_y:.2f}, Yaw={ self.current_yaw:.2f}')
 
+        if self.state == "IDLE" and self.pose_queue:
+            self.current_target = self.pose_queue[0]
+            self.state = "ROTATING"
+        
+        if self.state == "ROTATING":
+            target_yaw = 2 * math.atan2(self.current_target.pose.orientation.z, self.current_target.pose.orientation.w) # Compute the target yaw from the quaternion.
+            delta_angle = target_yaw - self.current_yaw
+            delta_angle = math.atan2(math.sin(delta_angle), math.cos(delta_angle))
+
+            if abs(delta_angle) > self.tolerance:
+                twist = Twist()
+                w = self.current_target.angular_velocity if delta_angle > 0 else -self.current_target.angular_velocity
+                twist.angular.z = w
+                self.cmd_pub.publish(twist)
+            else:
+                self.get_logger().info(f"Giro completado")
+                self.stop_robot()
+                self.state = "MOVING"
+        
+        if self.state == "MOVING":
+            target_x = self.current_target.pose.position.x
+            target_y = self.current_target.pose.position.y
+            distance = math.sqrt((target_x - self.current_x)**2 + (target_y - self.current_y)**2)
+
+            if distance > self.tolerance:
+                twist = Twist()
+                twist.linear.x = msg.linear_velocity  # Set the forward speed.
+                twist.angular.z = 0.0                  # No rotation during linear motion.
+                self.cmd_pub.publish(twist)
+            else:
+                self.get_logger().info(f"Avance completado a X = {target_x:.2f} y Y = {target_y:.2f}")
+                self.stop_robot()
+                self.pose_queue.pop(0)
+                self.current_target = None
+                self.state = "IDLE"
+
     def pose_callback(self, msg):
-        """
-        Callback function triggered upon receiving an ExtendedPose message.
-        
-        It extracts the target position (x, y) and orientation (yaw) from the message.
-        The controller then performs two actions:
-          1. Rotates the robot in place to match the desired yaw.
-          2. Moves the robot in a straight line to the target position.
-        """
-        # Extract target position from the message.
-        target_x = msg.pose.position.x
-        target_y = msg.pose.position.y
-        
-        # Compute the target yaw from the quaternion.
-        # Since only rotation around the Z-axis is assumed, only z and w components are used.
-        target_yaw = 2 * math.atan2(msg.pose.orientation.z, msg.pose.orientation.w)
-        
-        # Log the received segment with target pose and velocity commands.
-        self.get_logger().info(
-            f"Received segment: target=({target_x:.2f}, {target_y:.2f}), yaw={target_yaw:.2f} rad, "
-            f"v={msg.linear_velocity:.2f} m/s, w={msg.angular_velocity:.2f} rad/s, "
-            f"segment time={msg.time_stamp:.2f} s"
-        )
-        
-        # --------- 1. Rotation Phase -----------
-        # First, calculate the required rotation (delta_angle) to align the current orientation with target_yaw.
-        delta_angle = target_yaw - self.current_yaw
-        # Normalize the angle to be within the range of -pi to pi.
-        delta_angle = math.atan2(math.sin(delta_angle), math.cos(delta_angle))
+        self.pose_queue.append(msg)
+        self.get_logger().info("Nuevo waypoint recibido y agregado a la cola.")
 
-        tolerance = 0.05  # Umbral de tolerancia en radianes
-        
-        # Check if rotation is needed by comparing against a small threshold.
-        if abs(delta_angle) > tolerance:
-            # Determine the direction of rotation using the angular velocity provided in the message.
-            w = msg.angular_velocity if delta_angle >= 0 else -msg.angular_velocity
-            self.angle_pub.publish(w)
-            
-            # Create a Twist message to command the rotation.
-            twist = Twist()
-            twist.linear.x = 0.0         # No linear movement during rotation.
-            twist.angular.z = w          # Angular speed as computed.
-            
-            while abs(delta_angle) > tolerance:
-                self.cmd_pub.publish(twist)
-                delta_angle = target_yaw - self.current_yaw # Actualizamos delta_angle en cada iteración
-                delta_angle = math.atan2(math.sin(delta_angle), math.cos(delta_angle))
-                time.sleep(0.05) # Short sleep to allow repeated commands over time.
-
-            self.get_logger().info(f"Rotación completada a {target_yaw:.2f} rad.")
-            # After rotation, stop the robot.
-            self.stop_robot()
-
-        else:
-            self.get_logger().info("Rotation not required.")
-        
-        # --------- 2. Translation Phase -----------
-        # Compute the Euclidean distance to the target position.
-        distance = math.sqrt((target_x - self.current_x)**2 + (target_y - self.current_y)**2)
-        
-        # Check if movement is needed based on distance.
-        if distance > tolerance:
-            
-            # Create a Twist message to command the linear movement.
-            twist = Twist()
-            twist.linear.x = msg.linear_velocity  # Set the forward speed.
-            twist.angular.z = 0.0                  # No rotation during linear motion.
-
-            while distance > tolerance:
-                self.cmd_pub.publish(twist)
-                distance = math.sqrt((target_x - self.current_x)**2 + (target_y - self.current_y)**2)
-                time.sleep(0.05)          # Short sleep to allow repeated commands.
-
-            self.get_logger().info(f"Avance completado a X = {target_x:.2f} y Y = {target_y:.2f}")
-            # After moving, stop the robot.
-            self.stop_robot()
-
-        else:
-            self.get_logger().info("Linear movement not required.")
 
     def stop_robot(self):
         """
