c410cfcdebcd665773b26f026dcb1e7a6637b991
Gus Garciarreal <gusgarciarrealm@gmail.com>
2025-05-26 18:24:38 -0600

    Added line_follower package

---
+++ reto/src/line_follower/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'line_follower'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ggm',
+    maintainer_email='ggm@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'line_follower = line_follower.line_follower:main',
+        ],
+    },
+)
